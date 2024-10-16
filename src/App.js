68881b82d79bc09e9f1d7d691936c8480b8e6c14
army077 <adlrg077@gmail.com>
2024-10-16 12:13:27 -0600

    esconder el appbar si no esta logeado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   AppBar,
   Toolbar,
@@ -16,12 +16,29 @@ import Tecnicos from './codigo/tecnicos';
 import OrdenesTrabajoList from './codigo/ordenes_trabajo';
 import DetalleOrdenTrabajo from './codigo/detalle_orden';
 import Login from './codigo/login';
-import { AuthProvider } from './context/AuthContext';
+import { AuthProvider, useAuth } from './context/AuthContext';
 import PrivateRoute from './codigo/protected_route';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase/firebase-config';
 
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe; // Limpia el listener al desmontar
+  }, []);
+
+  if (loading) {
+    return <p>Cargando...</p>; // Mientras se carga el usuario
+  }
 
   const theme = createTheme({
     palette: {
@@ -38,40 +55,75 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Router>
-          <AppBar position="static" sx={{ backgroundColor: darkMode ? 'black' : 'black' }}>
-            <Toolbar>
-              <IconButton edge="start" color="inherit" aria-label="logo" component={RouterLink} to="/" style={{ marginRight: '20px' }}>
-                <img src={`/logo_asiarob.png`} alt="Logo" style={{ width: '160px', height: '40px' }} />
-              </IconButton>
+          {currentUser && ( // Condiciona la visibilidad del AppBar
+            <AppBar position="static" sx={{ backgroundColor: darkMode ? 'black' : 'black' }}>
+              <Toolbar>
+                <IconButton
+                  edge="start"
+                  color="inherit"
+                  aria-label="logo"
+                  component={RouterLink}
+                  to="/"
+                  style={{ marginRight: '20px' }}
+                >
+                  <img src={`/logo_asiarob.png`} alt="Logo" style={{ width: '160px', height: '40px' }} />
+                </IconButton>
 
-              {/* Título de la aplicación */}
-              <Typography variant="h6" style={{ flexGrow: 1 }}>
-                INSTALACIONES, MANTOS Y REPARACIONES
-              </Typography>
+                <Typography variant="h6" style={{ flexGrow: 1 }}>
+                  INSTALACIONES, MANTOS Y REPARACIONES
+                </Typography>
 
-              {/* Enlaces de navegación */}
-              <Link component={RouterLink} to="/" color="inherit" style={{ marginRight: '20px', textDecoration: "none" }} className='link-appbar'>
-                INICIO
-              </Link>
-              <Link component={RouterLink} to="/activities" color="inherit" style={{ marginRight: '20px', textDecoration: "none" }} className='link-appbar'>
-                ACTIVIDADES
-              </Link>
-              <Link component={RouterLink} to="/plan_trabajo" color="inherit" style={{ marginRight: '20px', textDecoration: "none" }} className='link-appbar'>
-                PLANES
-              </Link>
-              <Link component={RouterLink} to="/tecnico" color="inherit" style={{ marginRight: '20px', textDecoration: "none" }} className='link-appbar'>
-                CREAR ORDEN
-              </Link>
-              <Link component={RouterLink} to="/ordenes_trabajo" color="inherit" style={{ marginRight: '20px', textDecoration: "none" }} className='link-appbar'>
-                ORDENES
-              </Link>
+                <Link
+                  component={RouterLink}
+                  to="/"
+                  color="inherit"
+                  style={{ marginRight: '20px', textDecoration: 'none' }}
+                  className="link-appbar"
+                >
+                  INICIO
+                </Link>
+                <Link
+                  component={RouterLink}
+                  to="/activities"
+                  color="inherit"
+                  style={{ marginRight: '20px', textDecoration: 'none' }}
+                  className="link-appbar"
+                >
+                  ACTIVIDADES
+                </Link>
+                <Link
+                  component={RouterLink}
+                  to="/plan_trabajo"
+                  color="inherit"
+                  style={{ marginRight: '20px', textDecoration: 'none' }}
+                  className="link-appbar"
+                >
+                  PLANES
+                </Link>
+                <Link
+                  component={RouterLink}
+                  to="/tecnico"
+                  color="inherit"
+                  style={{ marginRight: '20px', textDecoration: 'none' }}
+                  className="link-appbar"
+                >
+                  CREAR ORDEN
+                </Link>
+                <Link
+                  component={RouterLink}
+                  to="/ordenes_trabajo"
+                  color="inherit"
+                  style={{ marginRight: '20px', textDecoration: 'none' }}
+                  className="link-appbar"
+                >
+                  ORDENES
+                </Link>
 
-              {/* Botón de cambio de tema */}
-              <p>Modo Oscuro</p>
-              <Switch checked={darkMode} onChange={handleThemeChange} color="default" />
-
-            </Toolbar>
-          </AppBar>
+                <p>Modo Oscuro</p>
+                <Switch checked={darkMode} onChange={handleThemeChange} color="default" />
+              </Toolbar>
+            </AppBar>
+          )}
 
           {/* Rutas de la aplicación */}
           <Routes>
