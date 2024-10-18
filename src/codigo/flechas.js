af226f07fc1e8e71b8b08006444279be668d7355
Matamorrozz <matamorrozz@hotmail.com>
2024-10-18 13:42:46 -0600

    acomodo

--- src/codigo/flechas.js
+++ src/codigo/flechas.js
@@ -1,32 +1,28 @@
 import React from 'react';
 
-// Flecha personalizada para "anterior"
 const PrevArrow = ({ className, style, onClick }) => (
   <div
     className={className}
     style={{
       ...style,
       display: 'block',
-      background: 'grey', // Cambia este color al que desees
+      background: 'grey', 
       borderRadius: '50%',
       marginRight: '10px',
-      // Opcional: un estilo redondeado
     }}
     onClick={onClick}
   />
 );
 
-// Flecha personalizada para "siguiente"
 const NextArrow = ({ className, style, onClick }) => (
   <div
     className={className}
     style={{
       ...style,
       display: 'block',
-      background: 'grey', // Cambia este color al que desees
+      background: 'grey', 
       borderRadius: '50%',
       marginRight: '20px',
-      // Opcional: un estilo redondeado
     }}
     onClick={onClick}
   />
--- src/context/protected_route.js
+++ src/context/protected_route.js
@@ -1,6 +1,6 @@
 // PrivateRoute.js
 import { Navigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "./AuthContext";
 
 const PrivateRoute = ({ children }) => {
   const { currentUser } = useAuth();
